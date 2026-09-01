5e58c3fe2f22d1afc9994b85fb35bd7f3d5a1405
André <andreluizalvesfernandes9@gmail.com>
2026-09-01 10:19:17 -0300

    atualização tela_jogo_heroi

---
+++ lib/tela_jogo_heroi.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TelaJogoHeroiState extends State<TelaJogoHeroi> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Tela jogo Heroi')));
+  }
+}
