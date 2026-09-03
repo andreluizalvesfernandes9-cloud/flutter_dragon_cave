fbc6958cb3b7b4c97e9c7b98532b3cf8f974fbd9
André <andreluizalvesfernandes9@gmail.com>
2026-09-03 10:19:39 -0300

    atualização da lista

--- lib/tela_jogo_heroi.dart
+++ lib/tela_jogo_heroi.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
+class TelaJogoHeroi extends StatefulWidget {
+  const TelaJogoHeroi({super.key});
+
+  @override
+  State<TelaJogoHeroi> createState() => TelaJogoHeroiState();
+}
+
+
 class TelaJogoHeroiState extends State<TelaJogoHeroi> {
   @override
   Widget build(BuildContext context) {
